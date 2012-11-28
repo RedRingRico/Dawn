96e0cb3ec89223bbe2ac8f9d3a74b494c71ef755
Rico <teh_programerer@interactivelaboratories.com>
2012-11-28 10:47:27 +0000

    |BUILD CONFIGURATION| [VS2003] Removed the /LTCG flag from the Release build [VS] Re-implemented the correct Dawn_*.rc file, version informtaion is now correct

--- Source/Windows/Headers/HgVersion.h
+++ Source/Windows/Headers/HgVersion.h
@@ -1,10 +1,10 @@
 #ifndef __DAWN_HGVERSION_HPP__
 #define __DAWN_HGVERSION_HPP__
 
-#define HG_REVISION				49
+#define HG_REVISION				51
 #define HG_LOCAL_MODIFICATIONS	1
-#define HG_CHANGESET			"aa7855edc8160fe8b31ad75d9021e6d213eb66fa"
-#define HG_CHANGESET_SHORT		"aa7855edc816"
+#define HG_CHANGESET			"d9fd37abdd6e39bdf944cacfc41dcf7ada3dcd95"
+#define HG_CHANGESET_SHORT		"d9fd37abdd6e"
 
 #endif
 
