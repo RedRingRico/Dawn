43b8197ea6736f13fa8929e878070939f7f0fadb
Rico <teh_programerer@interactivelaboratories.com>
2012-11-29 00:42:15 +0000

    |BUILD CONFIGURATION| [Pandora] Added the common source files to the build process
    |SOURCE|
    [Pandora::Time] Stubbed out the *Time* functions
    
    /TODO/
    [Pandora] Render a triangle

---
+++ Source/Pandora/Source/Time.cpp
@@ -0,0 +1,29 @@
+#include <Time.hpp>
+
+namespace Dawn
+{
+	D_BOOL StartTime( )
+	{
+		return D_TRUE;
+	}
+
+	D_FLOAT32 GetTimeS( )
+	{
+		return 0.0f;
+	}
+
+	D_FLOAT32 GetTimeMS( )
+	{
+		return 0.0f;
+	}
+
+	D_FLOAT32 GetTimeMiS( )
+	{
+		return 0.0f;
+	}
+
+	D_FLOAT32 GetTimeNS( )
+	{
+		return 0.0f;
+	}
+}
