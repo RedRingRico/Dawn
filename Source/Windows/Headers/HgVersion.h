364e7333b132777f6d74f4ea1cb924f1122a8edd
Rico <teh_programerer@interactivelaboratories.com>
2012-11-29 00:21:02 +0000

    |BUILD CONFIGURATION| [VS2010] Added VertexCache.hpp, GLVertexCache.[hpp/cpp]
    |SOURCE|
    [DataTypes] Fixed D_ERROR_GRAPHICS, VS2003 assumes int by default, so the issue was not caught before commiting (and lack of thinking also contributed)

--- Source/Windows/Headers/HgVersion.h
+++ Source/Windows/Headers/HgVersion.h
@@ -1,10 +1,10 @@
 #ifndef __DAWN_HGVERSION_HPP__
 #define __DAWN_HGVERSION_HPP__
 
-#define HG_REVISION				53
+#define HG_REVISION				54
 #define HG_LOCAL_MODIFICATIONS	1
-#define HG_CHANGESET			"7e6e4919dee1031d4b883a2777f726b2e0d853fa"
-#define HG_CHANGESET_SHORT		"7e6e4919dee1"
+#define HG_CHANGESET			"5419bfd88c0dc3e781aebb3e2ecf0591c5b2afdd"
+#define HG_CHANGESET_SHORT		"5419bfd88c0d"
 
 #endif
 
