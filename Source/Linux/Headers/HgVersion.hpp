4da2e5af3d7cdd9503ffcbd58c1100e2ff732c36
Rico <teh_programerer@interactivelaboratories.com>
2012-12-02 19:58:27 +0000

    |BUILD CONFIGURATION| [Pandora] Added a rule to generate Mercurial version information and made the mkdir commands quiet [Pandora] Added a shell script to generate Mercurial version information [Linux] Added a rule to generate Mercurial version information and made the mkdir commands quiet [Linux] Added a shell script to generate Mercurial version information

---
+++ Source/Linux/Headers/HgVersion.hpp
@@ -0,0 +1,10 @@
+#ifndef __DAWN_HGVERSION_HPP__
+#define __DAWN_HGVERSION_HPP__
+
+#define HG_REVISION            57
+#define HG_LOCAL_MODIFICATIONS 1
+#define HG_CHANGESET           ee7004270548e0155bdbdf63f99f9fe41ba303e7
+#define HG_CHANGESET_SHORT     ee7004270548
+
+#endif
+
