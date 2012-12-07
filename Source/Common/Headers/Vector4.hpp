a56ff56ae4cb5d1aed0665e5ef5b572588fe8d8e
Rico <teh_programerer@interactivelaboratories.com>
2012-12-07 01:28:20 +0000

    |SOURCE| [Pandora::Vector4] Implemented
    /TODO/
    [Matrix4x4] Declare class

--- Source/Common/Headers/Vector4.hpp
+++ Source/Common/Headers/Vector4.hpp
@@ -19,19 +19,19 @@ namespace Dawn
 		D_FLOAT32 Magnitude( ) const;
 		D_FLOAT32 MagnitudeSq( ) const;
 
-		D_FLOAT32 Dot( ) const;
+		D_FLOAT32 Dot( const Vector4 &p_Other ) const;
 
 		void Set( const D_FLOAT32 p_X, const D_FLOAT32 p_Y,
 			const D_FLOAT32 p_Z, const D_FLOAT32 p_W );
-		void SetX( const D_FLOAT32 p_X );
-		void SetY( const D_FLOAT32 p_Y );
-		void SetZ( const D_FLOAT32 p_Z );
-		void SetW( const D_FLOAT32 p_W );
+		D_INLINE void SetX( const D_FLOAT32 p_X ) { m_V[ 0 ] = p_X; }
+		D_INLINE void SetY( const D_FLOAT32 p_Y ) { m_V[ 1 ] = p_Y; }
+		D_INLINE void SetZ( const D_FLOAT32 p_Z ) { m_V[ 2 ] = p_Z; }
+		D_INLINE void SetW( const D_FLOAT32 p_W ) { m_V[ 3 ] = p_W; }
 
-		D_FLOAT32 GetX( ) const;
-		D_FLOAT32 GetY( ) const;
-		D_FLOAT32 GetZ( ) const;
-		D_FLOAT32 GetW( ) const;
+		D_INLINE D_FLOAT32 GetX( ) const { return m_V[ 0 ]; }
+		D_INLINE D_FLOAT32 GetY( ) const { return m_V[ 1 ]; }
+		D_INLINE D_FLOAT32 GetZ( ) const { return m_V[ 2 ]; }
+		D_INLINE D_FLOAT32 GetW( ) const { return m_V[ 3 ]; }
 
 		D_BOOL IsZero( ) const;
 		void Zero( );
@@ -53,6 +53,14 @@ namespace Dawn
 		Vector4 &operator*=( const Vector4 &p_Other );
 		Vector4 &operator*=( const D_FLOAT32 p_Scalar );
 		Vector4 &operator/=( const D_FLOAT32 p_Scalar );
+
+		// Accessor
+		D_FLOAT32 operator[ ]( const D_MEMSIZE p_Index ) const
+			{ return m_V[ p_Index ]; }
+
+		// Manipulator
+		D_FLOAT32 &operator[ ]( const D_MEMSIZE p_Index )
+			{ return m_V[ p_Index ]; }
 
 	private:
 		D_FLOAT32 m_V[ 4 ];
--- Source/Pandora/Headers/HgVersion.hpp
+++ Source/Pandora/Headers/HgVersion.hpp
@@ -1,10 +1,10 @@
 #ifndef __DAWN_HGVERSION_HPP__
 #define __DAWN_HGVERSION_HPP__
 
-#define HG_REVISION            62
+#define HG_REVISION            63
 #define HG_LOCAL_MODIFICATIONS 1
-#define HG_CHANGESET           "e4f96f1f28f9d40ce93c28f477cd3cefeb8f4133"
-#define HG_CHANGESET_SHORT     "e4f96f1f28f9"
+#define HG_CHANGESET           "383075c4caf748828e82ef8b9e165056331ad46b"
+#define HG_CHANGESET_SHORT     "383075c4caf7"
 
 #endif
 
